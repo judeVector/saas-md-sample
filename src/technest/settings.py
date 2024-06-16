501dedf19b31b04e4e1d5777f4a664f9938074b3
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-06-16 04:50:29 +0400

    Added Neon Postgres DB

--- src/technest/settings.py
+++ src/technest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e4twexto&!+%v7toget&wdm51z(bmnni6ftqv(cxtz6ee!jgpl"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-print("DEBUG", DEBUG, type(DEBUG), type(DEBUG))
+
+print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [".railway.app"]  # saas,prod.railway.app
 if DEBUG:
@@ -89,6 +92,19 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL, conn_health_checks=True, conn_max_age=CONN_MAX_AGE
+        )
+    }
 
 
 # Password validation

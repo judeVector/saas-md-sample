5997e59ba324d7ca945acf10fea7dc2745ee9028
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-06-20 04:34:35 +0400

    Prepare static files

--- src/technest/settings.py
+++ src/technest/settings.py
@@ -142,6 +142,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source(s) for python manage.py collecstatic
+
+STATICFILES_DIRS = [STATICFILES_BASE_DIR]
+
+# output for python manage.py collecstatic
+# local cdn
+
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
